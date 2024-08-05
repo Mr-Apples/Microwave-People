f89f7782888c8b4e86ad9d95dd2c34751ddd2f3d
apples <aidan@hethos.org>
2024-08-05 17:28:20 +1000

    Setup project

---
+++ src/main/java/org/hethos/MicrowavePeopleDataGenerator.java
@@ -0,0 +1,11 @@
+package org.hethos;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MicrowavePeopleDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
